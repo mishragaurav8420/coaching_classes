8a59016e0e10b93cd030a7a6e1b9ade2cb4eaff6
mishragaurav8420 <mishragaurav8420@gmail.com>
2025-02-18 00:22:50 +0530

    my_first_commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Add any JavaScript functionality you need here
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for contacting us! We will get back to you soon.');
+});
